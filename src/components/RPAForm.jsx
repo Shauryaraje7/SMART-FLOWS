b39ee986aeb308e091dbbcfedbda9849a005bf74
Shauryaraje7 <shauryaraje.23bce11724@vitbhopal.ac.in>
2025-07-31 17:43:47 +0530

    Version 2.0.4

--- src/components/RPAForm.jsx
+++ src/components/RPAForm.jsx
@@ -318,12 +318,8 @@ const handleSubmit = async (e) => {
               className="rpa-consultation-submit-btn"
               disabled={isSubmitting}
             >
-              {isSubmitting ? 'Submitting...' : 'Request Free Consultation'}
-              {!isSubmitting && (
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-              )}
+              {isSubmitting ? 'Submitting...' : 'Submit'}
+            
             </button>
             <p className="rpa-form-disclaimer">
               By submitting this form, you agree to our privacy policy. We'll never share your information without your permission.
