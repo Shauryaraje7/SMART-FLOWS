195e210a14418b2e13f2260e7901433e161e8228
Shauryaraje7 <shauryaraje.23bce11724@vitbhopal.ac.in>
2025-08-14 23:20:24 +0530

    version 12.2.1

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteSitemap from 'vite-plugin-sitemap'; 
 
-// https://vitejs.dev/config/
+const routes = [
+  { path: '/', lastmod: new Date() },
+  { path: '/services', lastmod: new Date() },
+  { path: '/courses',  lastmod: new Date() },
+  { path: '/blogpage',  lastmod: new Date() },
+  { path: '/aboutuspage',  lastmod: new Date() }
+];
+
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Ensure this is set correctly
+  plugins: [
+    react(),
+    viteSitemap({
+      baseUrl: 'http://smartflows.in/', // Must include protocol
+      routes: routes,
+      // Available options in v0.8.2:
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: true,
+      readable: true
+    })
+  ],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
-  },
-  server: {
-    port: 3000
+    emptyOutDir: true
   }
 });
