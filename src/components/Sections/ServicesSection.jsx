91c786d88818bc0ce18e8819dd4750da16e1bc94
Shauryaraje7 <shauryaraje.23bce11724@vitbhopal.ac.in>
2025-08-14 19:31:20 +0530

    version 12.0.6

--- src/components/Sections/ServicesSection.jsx
+++ src/components/Sections/ServicesSection.jsx
@@ -4,7 +4,7 @@ import '../styles/ServicesSection.css';
 import '../styles/Global.css';
 import UipathLogo from '../../assets/UiPath-Logo.png';
 import PowerAutomateLogo from '../../assets/power-automate-logo-removebg-preview.png';
-import AgenticAiLogo from '../../assets/aiagents.png';
+import AgenticAiLogo from '../../assets/newaiimg.png';
 import AutomationAnywher from '../../assets/Automation-Anywhere.png';
 import BluePrism from '../../assets/Blue-Prism.png';
 
